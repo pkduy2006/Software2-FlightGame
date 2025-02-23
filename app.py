5e1dad524d1a1bf819cd170d840b76c5e8ee19ea
pkduy2006 <pkduy2006@gmail.com>
2025-02-23 19:14:21 +0200

    Add initialize game part

--- app.py
+++ app.py
@@ -4,6 +4,16 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+import mysql.connector
+connection = mysql.connector.connect(
+    host='127.0.0.1',
+    port=3306,
+    database = 'FlightGame2',
+    user='root',
+    password='16102006',
+    autocommit=True
+)
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -11,6 +21,41 @@ def index():
 @app.route('/game')
 def game():
     return render_template('game.html')
+
+from geopy.distance import geodesic
+import random
+
+@app.route('/airports')
+def get_airports():
+    sql = """SELECT airport.name as 'name', airport.iso_country as 'iso_country', airport.municipality as 'municipality', country.name as
+'country', airport.ident as 'ident', airport.latitude_deg as 'latitude_deg', airport.longitude_deg as 'longitude_deg'
+    FROM airport, country
+    WHERE airport.iso_country = country.iso_country
+    AND airport.continent = 'EU'
+    AND airport.type = 'large_airport'
+    AND airport.iso_country NOT IN (SELECT airport.iso_country FROM airport WHERE iso_country = 'RU')
+    ORDER BY RAND()
+    LIMIT 40;"""
+    cursor = connection.cursor(dictionary = True)
+    cursor.execute(sql)
+    result = cursor.fetchall()
+
+    base = (result[0]['latitude_deg'], result[0]['longitude_deg'])
+    target = (result[1]['latitude_deg'], result[1]['longitude_deg'])
+    while geodesic(base, target).km < 2000:
+        random.shuffle(result)
+        base = (result[0]['latitude_deg'], result[0]['longitude_deg'])
+        target = (result[1]['latitude_deg'], result[1]['longitude_deg'])
+    
+    return result
+
+@app.route('/types')
+def get_types():
+    sql ="""SELECT * FROM types;"""
+    cursor = connection.cursor(dictionary = True)
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    return result
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True, host='127.0.0.1', port=5000)
--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,48 @@
 'use strict';
+
+async function getData(keywords) {
+    try {
+        const response = await fetch(`http://127.0.0.1:5000/${keywords}`);
+        if (!response.ok) throw new Error(`Failed to fetch data from http://127.0.0.1:5000/${keywords}`);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+async function initializeGame() {
+    const playerName = prompt('What is your name?');
+    document.querySelector('#player-name').innerHTML = `Name: ${playerName}`;
+    document.querySelector('#trip-distance').innerHTML = `Trip: 0 km`;
+    document.querySelector('#fuel-status').innerHTML = `Fuel: 1000 L`;
+    document.querySelector('#player-mission').innerHTML = 'Mission completed: No';
+
+
+    let airport_data = await getData('airports');
+    const types = await getData('types');
+    let airport_index = 0;
+    for (let i of types) {
+        for (let j = 0; j < i.total; j++) {
+            airport_data[airport_index].garrison = i.garrison;
+            airport_data[airport_index].storage = i.storage;
+            airport_index++;
+        }
+    }
+
+    document.querySelector('#name-base').innerHTML = airport_data[0].name;
+    document.querySelector('#municipality-base').innerHTML = airport_data[0].municipality;
+    document.querySelector('#country-base').innerHTML = airport_data[0].country;
+
+    document.querySelector('#name-target').innerHTML = airport_data[1].name;
+    document.querySelector('#municipality-target').innerHTML = airport_data[1].municipality;
+    document.querySelector('#country-target').innerHTML = airport_data[1].country;
+
+    document.querySelector('#name-location').innerHTML = airport_data[0].name;
+    document.querySelector('#municipality-location').innerHTML = airport_data[0].municipality;
+    document.querySelector('#country-location').innerHTML = airport_data[0].country;
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    initializeGame();
+});
