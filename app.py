c55240d29851f994cd515c02ae9abd1510d4e74b
pkduy2006 <pkduy2006@gmail.com>
2025-03-09 23:29:36 +0200

    Update the server

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
 from flask_cors import CORS
-
+import json
 app = Flask(__name__)
 CORS(app)
 
@@ -13,49 +13,109 @@ connection = mysql.connector.connect(
     password='16102006',
     autocommit=True
 )
-
 @app.route('/')
 def index():
     return render_template('index.html')
-
 @app.route('/game')
 def game():
     return render_template('game.html')
 
 from geopy.distance import geodesic
 import random
+@app.route('/player=<player_name>')
+def create_user(player_name):
+    try:
+        sql = """SELECT * FROM user_data WHERE name = %s;"""
+        cursor = connection.cursor(dictionary=True)
+        cursor.execute(sql, (player_name,))
+        player = cursor.fetchall()
 
-@app.route('/airports')
-def get_airports():
-    sql = """SELECT airport.name as 'name', airport.iso_country as 'iso_country', airport.municipality as 'municipality', country.name as
-'country', airport.ident as 'ident', airport.latitude_deg as 'latitude_deg', airport.longitude_deg as 'longitude_deg'
-    FROM airport, country
-    WHERE airport.iso_country = country.iso_country
-    AND airport.continent = 'EU'
-    AND airport.type = 'large_airport'
-    AND airport.iso_country NOT IN (SELECT airport.iso_country FROM airport WHERE iso_country = 'RU')
-    ORDER BY RAND()
-    LIMIT 40;"""
-    cursor = connection.cursor(dictionary = True)
-    cursor.execute(sql)
-    result = cursor.fetchall()
-
-    base = (result[0]['latitude_deg'], result[0]['longitude_deg'])
-    target = (result[1]['latitude_deg'], result[1]['longitude_deg'])
-    while geodesic(base, target).km < 2000:
-        random.shuffle(result)
-        base = (result[0]['latitude_deg'], result[0]['longitude_deg'])
-        target = (result[1]['latitude_deg'], result[1]['longitude_deg'])
-    
-    return result
-
-@app.route('/types')
-def get_types():
-    sql ="""SELECT * FROM types;"""
-    cursor = connection.cursor(dictionary = True)
-    cursor.execute(sql)
-    result = cursor.fetchall()
-    return result
+        if player:
+            response = {
+                "message": "This user already exists.",
+                "status": 200,
+            }
+            json_response = json.dumps(response)
+            http_response = Response(json_response, status=200, mimetype='application/json')
+            return http_response
+        else:
+            try:
+                sql = """SELECT airport.name as 'name', airport.iso_country as 'iso_country', airport.municipality as 'municipality', country.name as
+                'country', airport.ident as 'ident', airport.latitude_deg as 'latitude_deg', airport.longitude_deg as 'longitude_deg'
+                FROM airport, country
+                WHERE airport.iso_country = country.iso_country
+                AND airport.continent = 'EU'
+                AND airport.type = 'large_airport'
+                AND airport.iso_country NOT IN (SELECT airport.iso_country FROM airport WHERE iso_country = 'RU')
+                ORDER BY RAND()
+                LIMIT 40;"""
+                cursor.execute(sql)
+                airports = cursor.fetchall()
+                base = (airports[0]['latitude_deg'], airports[0]['longitude_deg'])
+                target = (airports[1]['latitude_deg'], airports[1]['longitude_deg'])
+                while geodesic(base, target).km < 2000:
+                    random.shuffle(airports)
+                    base = (airports[0]['latitude_deg'], airports[0]['longitude_deg'])
+                    target = (airports[1]['latitude_deg'], airports[1]['longitude_deg'])
+                try:
+                    sql = """SELECT * FROM types;"""
+                    cursor.execute(sql)
+                    types = cursor.fetchall()
+                    id = 0
+                    for type in types:
+                        for i in range(type["total"]):
+                            airports[id]["garrison"] = type["garrison"]
+                            airports[id]["storage"] = type["storage"]
+                            id = id + 1
+                    try:
+                        user = []
+                        user.append(player_name)
+                        for airport in airports:
+                            user.append(airport["ident"])
+                        user_tuple = tuple(user)
+                        #print(len(user_tuple))
+                        sql = """INSERT INTO user_data(name, base, target, airport3, airport4, airport5, airport6, airport7, airport8, airport9, airport10, airport11, airport12, airport13, airport14, airport15, airport16, airport17, airport18, airport19, airport20, airport21, airport22, airport23, airport24, airport25, airport26, airport27, airport28, airport29, airport30, airport31, airport32, airport33, airport34, airport35, airport36, airport37, airport38, airport39, airport40)
+                        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);"""
+                        cursor.execute(sql, user_tuple)
+                        response = {
+                            "message": "success",
+                            "status": 200,
+                        }
+                        json_response = json.dumps(response)
+                        http_response = Response(json_response, status=200, mimetype='application/json')
+                        return http_response
+                    except Exception:
+                        response = {
+                            "message": "failure",
+                            "status": 200
+                        }
+                        json_response = json.dumps(response)
+                        http_response = Response(json_response, status=200, mimetype='application/json')
+                        return http_response
+                except Exception:
+                    response = {
+                        "message": "There was an error setting airports' attributes.",
+                        "status": 500
+                    }
+                    json_response = json.dumps(response)
+                    http_response = Response(json_response, status=500, mimetype='application/json')
+                    return http_response
+            except Exception:
+                response = {
+                    "message": "There was an error retrieving airports.",
+                    "status": 500
+                }
+                json_response = json.dumps(response)
+                http_response = Response(json_response, status=500, mimetype='application/json')
+                return http_response
+    except Exception:
+        response = {
+            "message": "table user_data is not available",
+            "status": 500
+        }
+        json_response = json.dumps(response)
+        http_response = Response(json_response, status=500, mimetype='application/json')
+        return http_response
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True, host='127.0.0.1', port=5000)
--- static/js/main.js
+++ static/js/main.js
@@ -11,6 +11,11 @@ async function getData(keywords) {
     }
 }
 
+const redIcon = L.divIcon({className: 'red-icon'});
+const blackIcon = L.divIcon({className: 'black-icon'});
+
+let cur;
+
 async function initializeGame() {
     const playerName = prompt('What is your name?');
     document.querySelector('#player-name').innerHTML = `Name: ${playerName}`;
@@ -18,31 +23,55 @@ async function initializeGame() {
     document.querySelector('#fuel-status').innerHTML = `Fuel: 1000 L`;
     document.querySelector('#player-mission').innerHTML = 'Mission completed: No';
 
+    const airport_data = await getData('airports');
 
-    let airport_data = await getData('airports');
-    const types = await getData('types');
-    let airport_index = 0;
-    for (let i of types) {
-        for (let j = 0; j < i.total; j++) {
-            airport_data[airport_index].garrison = i.garrison;
-            airport_data[airport_index].storage = i.storage;
-            airport_index++;
+    document.querySelector('#name-base').innerHTML = `Name: ${airport_data[0].name}`;
+    document.querySelector('#municipality-base').innerHTML = `Municipality: ${airport_data[0].municipality}`;
+    document.querySelector('#country-base').innerHTML = `Country: ${airport_data[0].country}`;
+
+    document.querySelector('#name-target').innerHTML = `Name: ${airport_data[1].name}`;
+    document.querySelector('#municipality-target').innerHTML = `Municipality: ${airport_data[1].municipality}`;
+    document.querySelector('#country-target').innerHTML = `Country: ${airport_data[1].country}`;
+
+    document.querySelector('#name-location').innerHTML = `Name: ${airport_data[0].name}`;
+    document.querySelector('#municipality-location').innerHTML = `Municipality: ${airport_data[0].municipality}`;
+    document.querySelector('#country-location').innerHTML = `Country: ${airport_data[0].country}`;
+}
+
+function createMap() {
+    const airportMarkers = L.featureGroup().addTo(map);
+    for (let i = 0; i < airports.length; i++) {
+        const marker = L.marker([airports[i].latitude_deg, airports[i].longitude_deg]).addTo(map);
+        airportMarkers.addLayer(marker);
+        airports[i].active = true;
+        if (i === 0)
+            marker.setIcon(redIcon);
+        if (i === 1)
+            marker.setIcon(blackIcon);
+
+        if(i === cur) {
+            marker.bindPopup(`You are here <b>${airports[i].name}</b>`);
+            marker.openPopup();
+        } else {
+            const div = document.createElement('div');
+            const h4 = document.createElement('h4');
+            h4.innerHTML = airports[i].name;
+            div.append(h4);
+            const p = document.createElement('p');
+
         }
+
     }
-
-    document.querySelector('#name-base').innerHTML = airport_data[0].name;
-    document.querySelector('#municipality-base').innerHTML = airport_data[0].municipality;
-    document.querySelector('#country-base').innerHTML = airport_data[0].country;
-
-    document.querySelector('#name-target').innerHTML = airport_data[1].name;
-    document.querySelector('#municipality-target').innerHTML = airport_data[1].municipality;
-    document.querySelector('#country-target').innerHTML = airport_data[1].country;
-
-    document.querySelector('#name-location').innerHTML = airport_data[0].name;
-    document.querySelector('#municipality-location').innerHTML = airport_data[0].municipality;
-    document.querySelector('#country-location').innerHTML = airport_data[0].country;
 }
 
 document.addEventListener('DOMContentLoaded', (event) => {
     initializeGame();
+    cur = 0;
+
+    const map = L.map('map', {tap: false});
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 20,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+    createMap();
 });
